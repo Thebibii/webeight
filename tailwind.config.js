1f038634dfe8c390af5b16bbbfabc6c59beee38a
habibiesanji@gmail.com <habibiesanji@gmail.com>
2022-10-23 14:09:48 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'poppins': 'Poppins',
+      },
+      colors: {
+        'primary': '#71717a',
+      },
+      backgroundPosition: {
+        'center1' : '55% 50%',
+      }
+    },
+  },
+  plugins: [],
+}
